e18dd1d95c6927fabade7c5242e8e09e739d4a0c
Thái Minh <thaiminh30082006@gmail.com>
2025-10-07 23:57:57 +0700

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,24 @@
+<?php
+$ten = "Nguyen thai minh";
+$Tuoi ="20";
+$Diachi ="Hanoi";
+$email ="thaiminh30082006@Gmail.com";
+$sdt ="0867243442";
+echo"
+<table border='1' width='50%'>
+<tr>
+<th> Ten</th>
+<th> Tuoi</th>
+<th> Dia chi</th>
+<th> Email</th>
+<th> SDT</th>
+</tr>
+<tr>
+<td> $ten</td>
+<td> $Tuoi</td>
+<td> $Diachi</td>
+<td> $email</td>
+<td> $sdt</td>
+<tr>
+</table>";
+?>
